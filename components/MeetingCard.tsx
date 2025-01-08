3896f389e49ef0f58ad12341fca371831492d138
tsikot39 <corpusjohnbenedict@gmail.com>
2025-01-07 21:14:29 -0700

    final version

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import Image from "next/image";
+import Image from 'next/image';
 
-import { cn } from "@/lib/utils";
-import { Button } from "./ui/button";
-import { avatarImages } from "@/constants";
-import { useToast } from "./ui/use-toast";
+import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+// import { avatarImages } from '@/constants';
+import { useToast } from './ui/use-toast';
 
 interface MeetingCardProps {
   title: string;
@@ -31,7 +31,7 @@ const MeetingCard = ({
   const { toast } = useToast();
 
   return (
-    <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
+    <section className="flex min-h-[158px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
       <article className="flex flex-col gap-5">
         <Image src={icon} alt="upcoming" width={28} height={28} />
         <div className="flex justify-between">
@@ -41,8 +41,8 @@ const MeetingCard = ({
           </div>
         </div>
       </article>
-      <article className={cn("flex justify-center relative", {})}>
-        <div className="relative flex w-full max-sm:hidden">
+      <article className={cn('flex justify-center relative', {})}>
+        {/* <div className="relative flex w-full max-sm:hidden">
           {avatarImages.map((img, index) => (
             <Image
               key={index}
@@ -57,7 +57,7 @@ const MeetingCard = ({
           <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
           </div>
-        </div>
+        </div> */}
         {!isPreviousMeeting && (
           <div className="flex gap-2">
             <Button onClick={handleClick} className="rounded bg-blue-1 px-6">
@@ -70,7 +70,7 @@ const MeetingCard = ({
               onClick={() => {
                 navigator.clipboard.writeText(link);
                 toast({
-                  title: "Link Copied",
+                  title: 'Link Copied',
                 });
               }}
               className="bg-dark-4 px-6"
